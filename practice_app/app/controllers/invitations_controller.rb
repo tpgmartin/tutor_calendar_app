33182de1c3a8edc8a4612a063adc6423f5b13770
Thomas Martin <tpgmartin@gmail.com>
2014-03-21 17:28:35 +0000

    Basic invite email method - produces standard email for all users

--- practice_app/app/controllers/invitations_controller.rb
+++ practice_app/app/controllers/invitations_controller.rb
@@ -8,6 +8,7 @@ class InvitationsController < ApplicationController
     @invitation = Invitation.new(params[:invitation])
     @invitation.sender = current_user
     if @invitation.save
+      UserMailer.invite_email(@invitation, signup_url(@invitation.token)).deliver
       # UserMailer.deliver(@invitation, signup_url(@invitation.token))
       flash[:notice] = "Thank you, invitation sent."
       redirect_to current_user
--- practice_app/app/mailers/user_mailer.rb
+++ practice_app/app/mailers/user_mailer.rb
@@ -11,11 +11,17 @@ class UserMailer < ActionMailer::Base
     mail to: user.email, subject: "Sign Up Confirmation"
   end
 
-  def invitation(invitation, signup_url)
-    subject    'Invitation'
-    recipients invitation.recipient_email
-    from       'foo@example.com'
-    body       :invitation => invitation, :signup_url => signup_url
-    invitation.update_attribute(:sent_at, Time.now)
+  def invite_email(invitation, signup_url)
+    @user = invitation
+    @sign_url  = signup_url
+    mail to: invitation.recipient_email, subject: "Invitation"
   end
+
+  # def invitation(invitation, signup_url)
+  #   subject    'Invitation'
+  #   recipients invitation.recipient_email
+  #   from       'foo@example.com'
+  #   body       :invitation => invitation, :signup_url => signup_url
+  #   invitation.update_attribute(:sent_at, Time.now)
+  # end
 end
